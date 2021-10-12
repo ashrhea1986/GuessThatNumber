92a552d2a94a2864866311f43d3e7cdf220747ea
“ashrhea1986” <ashleyrhea1986@yahoo.com>
2021-10-12 10:15:36 -0500

    Created starting variables for game.

---
+++ guess.js
@@ -0,0 +1,21 @@
+//  GUESS THAT NUMBER 
+// Message to be used throughout file/project
+const enterNumText = `Please enter a number greater than zero`;
+
+// For restarting game
+let restartGame = true;
+
+// For storing the range of the number to be guessed
+let rangeNum;
+
+// For storing the number to be guessed
+let randomNum;
+
+// For storing the number of attempts that user has left
+let attempts;
+
+// For storing user's guess
+let guess;
+
+// For storing user's response to play again or not play again
+let playAgain;
